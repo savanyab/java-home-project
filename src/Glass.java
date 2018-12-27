641c1de412657a31cd69a3374315827d96862512
savanyab <savanyab@gmail.com>
2018-12-27 19:20:09 +0100

    added try-catch block to Glass purchaseRawMaterial()

--- src/Glass.java
+++ src/Glass.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Glass extends RawMaterial {
@@ -7,12 +8,30 @@ public class Glass extends RawMaterial {
     public void purchaseRawMaterial() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Mennyi üveget vegyünk?");
-        int a = sc.nextInt();
-        capital -= a * purchasePrice;
-        quantity += a;
-        System.out.println("üveg mennyiség: " + quantity);
-        setExpenses(a * purchasePrice);
-        System.out.println("üveg expenses: " + getExpenses());
+
+        try {
+            int a = Integer.parseInt(sc.next());
+            if (a >= 0 && a * purchasePrice <= capital) {
+                capital -= a * purchasePrice;
+                quantity += a;
+                System.out.println("üveg mennyiség: " + quantity);
+                setExpenses(a * purchasePrice);
+                System.out.println("üveg expenses: " + getExpenses());
+            } else if (a * purchasePrice > capital) {
+                System.out.println("A rendelkezésre álló tőke " + capital + ". " +
+                        a + " egység üveg beszerzése " + a * purchasePrice + " összegbe kerülne. Kevesebb üveget rendelj!");
+                purchaseRawMaterial();
+            } else {
+                System.out.println("Pozitív egész számot vagy nullát adj meg!");
+                purchaseRawMaterial();
+            }
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.out.println("Pozitív egész számot vagy nullát írj be!");
+            purchaseRawMaterial();
+        }
+
+
+
     }
 
     @Override
